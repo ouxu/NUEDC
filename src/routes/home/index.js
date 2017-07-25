7760e5e9f333a08b2242d33fe73ca726ccff7928
贾欣蕊 <jiaxinrui@hrsoft.net>
2017-07-25 14:54:54 +0800

    【story=53】首页footer添加
    Change-Id: I85ac70a11ee428cd108f4479834e119bb066b56b

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import './index.less'
 import HomeBanner from './Banner/Banner'
 import ShowItems from './ShowItems'
+import Footer from './Footer'
 const HomePage = () => {
   return (
     <div className='home-page'>
-
       <HomeBanner />
       <ShowItems />
+      <Footer />
     </div>
   )
 }
